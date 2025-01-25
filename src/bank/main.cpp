d86bce1077ff1a7a31ec3279a01de9d11090f979
CarlosPavajeau <cantte098@gmail.com>
2025-01-24 20:52:57 -0500

    chore: update example

--- src/bank/main.cpp
+++ src/bank/main.cpp
@@ -1,54 +1,58 @@
+#include <iomanip>
 #include <iostream>
 
 #include "db_connection.h"
+#include "field.h"
+#include "query_result.h"
 #include "sqlite3.h"
-
-struct test_result
-{
-  int id;
-};
-
-int test_db_callback(void* data, const int argc, char** argv, char** azColName)
-{
-  printf("sqlite callback\n");
-  for (int i = 0; i < argc; i++) {
-    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-  }
-  printf("\n");
-
-  const auto result = static_cast<test_result*>(data);
-  result->id = std::atoi(argv[0]);
-
-  return 0;
-}
 
 int main()
 {
-  sqlite3* db;
+  bank::db::db_connection db_connection("test.db");
 
-  const int open_result = sqlite3_open("test.db", &db);
-  if (open_result != SQLITE_OK) {
-    std::cout << sqlite3_errmsg(db) << std::endl;
-    sqlite3_close(db);
+  if (!db_connection.open()) {
+    std::cerr << "Failed to open database connection" << std::endl;
     return EXIT_FAILURE;
   }
 
-  test_result result {};
-  const int exec_result = sqlite3_exec(db,
-                                       "SELECT id FROM test where id = 1",
-                                       test_db_callback,
-                                       &result,
-                                       nullptr);
-
-  if (exec_result != SQLITE_OK) {
-    std::cout << sqlite3_errmsg(db) << std::endl;
-    sqlite3_close(db);
+  if (const int create_table_result = db_connection.execute(
+          "CREATE TABLE IF NOT EXISTS test(id int, name VARCHAR(32))");
+      create_table_result != SQLITE_OK)
+  {
+    db_connection.close();
     return EXIT_FAILURE;
   }
 
-  std::cout << "Resul is: " << result.id << std::endl;
+  if (const int delete_all_result = db_connection.execute("DELETE FROM test");
+      delete_all_result != SQLITE_OK)
+  {
+    db_connection.close();
+    return EXIT_FAILURE;
+  }
 
-  sqlite3_close(db);
+  if (const int insert_result = db_connection.execute(
+          R"(INSERT INTO test VALUES (1, "Namono"), (2, "Pedro"))");
+      insert_result != SQLITE_OK)
+  {
+    db_connection.close();
+    return EXIT_FAILURE;
+  }
+
+  const auto query_result = db_connection.query("SELECT * FROM test");
+
+  while (query_result->next()) {
+    const auto row = query_result->fetch();
+
+    const int id = row[0].get_int();
+    const auto name = row[1].get_string();
+
+    std::cout << std::string(15, '*') << std::endl;
+    std::cout << "data read" << std::endl;
+    std::cout << "id: " << id << std::endl;
+    std::cout << "name: " << name << std::endl;
+  }
+
+  db_connection.close();
 
   return 0;
 }
