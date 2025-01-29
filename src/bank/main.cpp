0abb28997cf58445dd9160adb68a140f8a3930a3
CarlosPavajeau <cantte098@gmail.com>
2025-01-29 11:54:33 -0500

    chore: update main file to include db update

--- src/bank/main.cpp
+++ src/bank/main.cpp
@@ -1,9 +1,11 @@
+#include <format>
 #include <iomanip>
 #include <iostream>
 
 #include <sqlite3.h>
 
 #include "db_connection.h"
+#include "db_updater.h"
 #include "field.h"
 #include "git_revision.h"
 #include "prepared_statement.h"
@@ -11,37 +13,16 @@
 
 int main()
 {
+  std::cout << "[INFO]: git hash: " << git_revision::get_hash() << std::endl;
+
   bank::db::db_connection db_connection("test.db");
 
-  std::cout << "git hash: " << git_revision::get_hash() << std::endl;
-
   if (!db_connection.open()) {
-    std::cerr << "Failed to open database connection" << std::endl;
+    std::cerr << "[ERROR]: failed to open database connection" << std::endl;
     return EXIT_FAILURE;
   }
 
-  if (const int create_table_result = db_connection.execute(
-          "CREATE TABLE IF NOT EXISTS test(id int, name VARCHAR(32))");
-      create_table_result != SQLITE_OK)
-  {
-    db_connection.close();
-    return EXIT_FAILURE;
-  }
-
-  if (const int delete_all_result = db_connection.execute("DELETE FROM test");
-      delete_all_result != SQLITE_OK)
-  {
-    db_connection.close();
-    return EXIT_FAILURE;
-  }
-
-  if (const int insert_result = db_connection.execute(
-          R"(INSERT INTO test VALUES (1, "Namono"), (2, "Pedro"))");
-      insert_result != SQLITE_OK)
-  {
-    db_connection.close();
-    return EXIT_FAILURE;
-  }
+  bank::db::db_updater::update(db_connection);
 
   db_connection.prepare_statements();
 
@@ -57,10 +38,12 @@ int main()
     const int id = row[0].get_int();
     const auto name = row[1].get_string();
 
-    std::cout << std::string(15, '*') << std::endl;
-    std::cout << "data read" << std::endl;
-    std::cout << "id: " << id << std::endl;
-    std::cout << "name: " << name << std::endl;
+    std::cout << std::format("[INFO]: data read \n{}\n id: {}\n name: {}\n{}",
+                             std::string(15, '*'),
+                             id,
+                             name,
+                             std::string(15, '*'))
+              << std::endl;
   }
 
   db_connection.close();
