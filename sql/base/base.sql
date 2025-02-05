ea8da8600447fac171b91dd2a7de3f88a97cf1f8
CarlosPavajeau <cantte098@gmail.com>
2025-02-05 09:57:08 -0500

    refactor: remove account_balance table

--- sql/base/base.sql
+++ sql/base/base.sql
@@ -15,22 +15,12 @@ CREATE TABLE IF NOT EXISTS account
     username   VARCHAR(32)  NOT NULL,
     password   VARCHAR(128) NOT NULL,
     email      VARCHAR(128) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_DATE,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_DATE,
+
+    balance    DOUBLE       NOT NULL DEFAULT 0,
 
     CONSTRAINT unique_username UNIQUE (username),
     CONSTRAINT unique_email UNIQUE (email)
-);
-
-DROP TABLE IF EXISTS account_balance;
-
-CREATE TABLE IF NOT EXISTS account_balance
-(
-    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    balance    DOUBLE  NOT NULL DEFAULT 0,
-
-    account_id INTEGER NOT NULL,
-
-    CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (id)
 );
 
 DROP TABLE IF EXISTS account_transaction;
