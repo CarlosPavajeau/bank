d913f4aab212bbaed49d5756d918b015fe4b8aa8
CarlosPavajeau <cantte098@gmail.com>
2025-02-05 10:17:47 -0500

    feat: add account constructors

--- src/bank/entities/account.h
+++ src/bank/entities/account.h
@@ -1,6 +1,7 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 #include <string>
+#include <utility>
 
 #include "define.h"
 
@@ -8,19 +9,37 @@ namespace bank::entities
 {
 struct account
 {
-  uint64 id;
+  account() = default;
+
+  account(const uint64 id,
+          std::string username,
+          std::string password,
+          std::string email,
+          const double balance,
+          const uint64 created_at)
+      : id(id)
+      , username(std::move(username))
+      , password(std::move(password))
+      , email(std::move(email))
+      , balance(balance)
+      , created_at(created_at)
+  {
+  }
+
+  account(std::string username, std::string password, std::string email)
+      : username(std::move(username))
+      , password(std::move(password))
+      , email(std::move(email))
+  {
+  }
+
+  uint64 id = 0;
   std::string username;
   std::string password;
   std::string email;
-  uint64 created_at;
-};
+  double balance = 0.0;
 
-struct account_balance
-{
-  uint64 id;
-  double balance;
-
-  std::string account_id;
+  uint64 created_at = 0;
 };
 
 struct account_transaction
