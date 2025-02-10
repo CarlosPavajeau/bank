49232f522f19f430c635179baaf03ae21aff7b0c
CarlosPavajeau <cantte098@gmail.com>
2025-02-10 12:51:33 -0500

    feat: add method to populate database

--- src/database/updater/db_updater.cpp
+++ src/database/updater/db_updater.cpp
@@ -1,9 +1,10 @@
 #include <format>
-#include <iostream>
+#include <fstream>
 
 #include "db_updater.h"
 
 #include "db_connection.h"
+#include "field.h"
 #include "git_revision.h"
 #include "logger.h"
 #include "query_result.h"
@@ -11,6 +12,41 @@
 
 namespace bank::db
 {
+bool db_updater::populate(const db_connection& connection)
+{
+  const auto result = retrieve(
+      connection,
+      R"(SELECT COUNT(*) tables FROM sqlite_master WHERE type = "table" AND name != "sqlite_sequence")");
+
+  if (result && result->next()) {
+    const auto field = result->fetch();
+
+    if (const auto tables = field[0].get_uint64(); tables > 0) {
+      return true;
+    }
+  }
+
+  LOG_INFO("populating database...");
+
+  const std::string base_file = get_base_file();
+  const path base(base_file);
+
+  if (!exists(base)) {
+    LOG_INFO("base file not found");
+    return false;
+  }
+
+  try {
+    apply_file(connection, base);
+  } catch (const std::exception&) {
+    return false;
+  }
+
+  LOG_INFO("database populated");
+
+  return true;
+}
+
 bool db_updater::update(const db_connection& connection)
 {
   const path source_directory =
@@ -44,6 +80,12 @@ bool db_updater::update(const db_connection& connection)
   return true;
 }
 
+std::string db_updater::get_base_file()
+{
+  return std::format(
+      "{}{}", git_revision::get_source_directory(), "/sql/base/base.sql");
+}
+
 query_result db_updater::retrieve(const db_connection& connection,
                                   const std::string_view query)
 {
@@ -56,6 +98,22 @@ void db_updater::apply(const db_connection& connection,
                        const std::string_view query)
 {
   [[maybe_unused]] auto _ = connection.execute(query);
+}
+
+void db_updater::apply_file(const db_connection& connection, const path& path)
+{
+  std::ifstream file(path.generic_string());
+
+  if (!file.is_open()) {
+    throw std::runtime_error("file could not be openend");
+  }
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+
+  const auto query = buffer.str();
+
+  apply(connection, query);
 }
 
 }  // namespace bank::db
--- src/database/updater/db_updater.h
+++ src/database/updater/db_updater.h
@@ -10,14 +10,18 @@ namespace bank::db
 class db_updater
 {
 public:
+  static bool populate(const db_connection& connection);
   static bool update(const db_connection& connection);
 
 private:
   typedef std::filesystem::path path;
 
+  static std::string get_base_file();
+
   static query_result retrieve(const db_connection& connection,
                                std::string_view query);
   static void apply(const db_connection& connection, std::string_view query);
+  static void apply_file(const db_connection& connection, const path& path);
 };
 }  // namespace bank::db
 
